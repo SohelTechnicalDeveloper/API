2e3b22324a1ac24c2007bdd25cd79febed9288e3
SohelTechnicalDeveloper <154615472+SohelTechnicalDeveloper@users.noreply.github.com>
2024-04-02 11:15:21 +0530

    change kar do

--- data.js
+++ data.js
@@ -568,24 +568,21 @@ var data = {
         //    console.log(data.products[0].images)
       
 
-  var product_list = data.products ;
+  //var product_list = data.products ;
   
 
-  let html = "";
+  //let html = "";
    
 //    for(let i=0;i<product_list.length;i++)
-
 //    {
-
 //       html+=`<li> Title = ${product_list[i].title}<br>  Brand = ${product_list[i].brand}<br>  Price = ${product_list[i].price }<br> Rating : ${product_list[i].rating} <br> `
-     
-//       document.getElementById('productList').innerHTML = html;
+//           document.getElementById('productList').innerHTML = html;
 //     }
 
 
-  html = product_list.map( item => `<li id="Product"> Title = ${item.title} <br>  Brand = ${item.brand}<br>  Price = ${item.price}<br> Rating : ${item.rating} <br> Category = ${item.category} <img src="${item.thumbnail}">`)
+      //  html = product_list.map( item => `<li id="Product"> Title = ${item.title} <br>  Brand = ${item.brand}<br>  Price = ${item.price}<br> Rating : ${item.rating} <br> Category = ${item.category} <img src="${item.thumbnail}">`)
     
-       document.getElementById('productList').innerHTML = html;
+      //  document.getElementById('productList').innerHTML = html;
 
 
 // const  searchProductbybrand = (item) =>
@@ -604,12 +601,6 @@ var data = {
  
 //     html+=`<li> Title = ${product_list[i].title}<br>  Brand = ${product_list[i].brand}<br>  Price = ${product_list[i].price }<br> Rating : ${product_list[i].rating} <br> `
 //     document.getElementById('productList').innerHTML = html;
-    
-
-
-    
-   
-   
    
    
 //    const searchProductByPirce =(item)=> 
@@ -631,3 +622,65 @@ var data = {
 //    let temp= product_list.filter(searchProductbybrand);
 
 //       console.log(temp)
+
+
+// var product_list = data.products;
+ 
+//  let html ="";
+
+//  for(let i=0;i<product_list.length;i++)
+//  {
+//     html +=`<li> ${product_list[i].brand} , ${product_list[i].category} , ${product_list[i].price}</li>`
+//  }
+
+  // const searchProductByPirce = item => item.price< 500;
+        
+  // product_list = product_list.filter( (item) =>{ return item.price< 500 })
+
+
+//  html =   product_list.filter( (item) =>{ return item.price< 500 }) .map((item)=>{
+
+//      return `<li> ${item.brand} , ${item.price} , ${item.category} </li>`
+//   })
+//   document.getElementById('productList').innerHTML =  product_list.filter( (item) =>{ return item.price< 500 }) .map((item)=>{
+
+//     return `<li> ${item.brand} , ${item.price} , ${item.category} </li>`
+//  });
+
+var product_list = data.products;
+ 
+ let html ="";
+
+//   let users = [];
+//  const searchInput = document.querySelector('#input');
+  
+//   searchInput.addEventListener('input',e =>{
+   
+//      const value = e.target.value;
+//     //  console.log(html);
+//     users.forEach(item => 
+//     {
+//       const isVisible = item.brand.includes(value);
+//        item.element.productList.toggle("hide",!isVisible)
+//     });
+//  })
+
+
+
+const searchProductByPrice = ((item)=>{
+   
+    if(item.price<500)
+    {
+       return true; 
+
+    }
+});
+
+ product_list = product_list.filter(searchProductByPrice)
+  
+   users = product_list.map((item)=>{
+     
+       return `<li> ${item.brand},${item.price} </li>`
+  })
+
+document.getElementById('productList').innerHTML = users;
